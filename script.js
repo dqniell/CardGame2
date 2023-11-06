7e842306ff2ab4751909bfe2037578a1187376ac
Dariya <dariyab@nycstudents.net>
2023-11-05 22:29:25 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const card1=document.getElementsById("card1");
+var matches=0;
+var attempts=0;
+var currCardsFlipped=0;
+
+card1.addEventListener("click", flipcard())
+
+
+card.addEventListener("click", flipCard());
+
+function flipCard(){
+    
+}
